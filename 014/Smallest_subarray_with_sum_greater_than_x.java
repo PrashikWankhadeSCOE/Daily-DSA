f65e03c37cb26d6fb3577e66bff4b56e18efd7ab
PrashikWankhadeSCOE <prashikwankhade68@gmail.com>
2024-01-28 21:57:32 +0530

    Day 14

--- 014/Smallest_subarray_with_sum_greater_than_x.java
+++ 014/Smallest_subarray_with_sum_greater_than_x.java
@@ -24,3 +24,30 @@ class Solution {
         
     }
 }
+
+//! TC == O(N^2)
+//! SC == O(1)
+
+class Solution1 {
+
+    public static int smallestSubWithSum(int arr[], int n, int x) {
+        int min = n+1;
+        int sum = 0;
+        int start = 0;
+        int end = 0;
+        while(end<n){
+            while(sum<=x && end < n){
+                sum+=arr[end++];
+            }
+            while(sum>x && start < n){
+                if(end-start<min)
+                    min = end-start;
+                sum-=arr[start++];
+            }
+        }
+        if(min==n+1)
+            return 0;
+        return min;
+    }
+}
+
