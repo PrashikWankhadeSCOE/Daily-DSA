5c731cbdc9b2f4440e3e4f7870965b2d7ac01d28
PrashikWankhadeSCOE <prashikwankhade68@gmail.com>
2024-02-20 21:54:03 +0530

    Day 36

--- 036/11. Container With Most Water.java
+++ 036/11. Container With Most Water.java
@@ -19,3 +19,32 @@ class Solution {
         return area;
     }
 }
+
+//! TC == O(N)
+//! SC == O(1)
+
+class Solution1 {
+    public int maxArea(int[] height) {
+        int s = 0;
+        int e = height.length-1;
+        int max = 0;
+        while(s<e){
+            
+            int se = height[s];
+            int ee = height[e];
+            int area = Math.min(se,ee)*(e-s);
+            
+            if(max<area){
+                max = area;
+            }
+            if(se<ee){
+                s++;
+            }
+            else{
+                e--;
+            }
+            
+        }
+        return max;
+    }
+}
